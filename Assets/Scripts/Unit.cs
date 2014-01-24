82741a8c522668cc2aa49a2e5eb80d9e325ee5a0
craiglonsdale <deftoned@gmail.com>
2014-01-25 02:27:14 +0800

    Added some forgotten files

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Unit : MonoBehaviour
+{
+	public float movementSpeed = 3.0f;
+	protected CharacterController control;
+	protected Vector3 move = Vector3.zero;
+
+	// Use this for initialization
+	public virtual void Start ()
+	{
+		control = GetComponent<CharacterController>();
+		if(!control)
+		{
+			Debug.LogError ("Unit.Start() " + name + " has no character controller");
+			enabled = false;
+		}
+	}
+
+	// Update is called once per frame
+	public virtual void Update ()
+	{
+		control.SimpleMove(move * movementSpeed);
+	}
+}
---
+++ Assets/Scripts/UnitPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitPlayer : Unit
+{
+
+	// Use this for initialization
+	public override void Start ()
+	{
+		base.Start();
+	}
+
+	// Update is called once per frame
+	public override void Update ()
+	{
+
+		//Movement shiat
+		move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+		move.Normalize();
+
+		base.Update();
+	}
+}
