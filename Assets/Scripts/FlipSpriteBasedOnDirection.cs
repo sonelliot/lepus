fb65056e4f6793a583550748d3369c25469c86ce
John Hannagan <weetbix_@hotmail.com>
2014-01-25 14:00:55 +0800

    FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

---
+++ Assets/Scripts/FlipSpriteBasedOnDirection.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipSpriteBasedOnDirection : MonoBehaviour
+{
+		private Vector3 LastPosition;
+
+		private float MIN_MOVE_DISTANCE = 0.1f;
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				NetworkView NV = gameObject.GetComponent<NetworkView> ();
+
+				if (NV == null || NV.isMine) {
+						if (LastPosition != null) {
+
+								float moveAmount = (transform.position - LastPosition).magnitude;
+
+								if (moveAmount > MIN_MOVE_DISTANCE) {
+										if (transform.position.x < LastPosition.x) {
+												transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+										} else {
+												transform.localScale = new Vector3 (-1.0f, 1.0f, 1.0f);
+										}
+								}
+	
+						}
+						LastPosition = transform.position;
+				}
+		}
+}
