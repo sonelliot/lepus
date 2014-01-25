e5e7738f422d9478ecf1ff4e14b4a5fd8b2cb970
John Hannagan <weetbix_@hotmail.com>
2014-01-25 10:59:02 +0800

    Fix Knife bug

--- Assets/Scripts/KnifeyScript.cs
+++ Assets/Scripts/KnifeyScript.cs
@@ -6,14 +6,17 @@ public class KnifeyScript : MonoBehaviour
 {
     void Start ()
     {
-        var mouse_pos = Input.mousePosition;
-        var object_pos = Camera.main.WorldToScreenPoint (transform.position);
-        mouse_pos.x = mouse_pos.x - object_pos.x;
-        mouse_pos.y = mouse_pos.y - object_pos.y;
-        
-        //rigidbody2D.transform.rotation = transform.rotation;
-        rigidbody2D.AddForce (new Vector2 (mouse_pos.x * 500f,
-                                           mouse_pos.y * 500f));
+        if (GetComponent<NetworkView> ().isMine)
+        {
+            var mouse_pos = Input.mousePosition;
+            var object_pos = Camera.main.WorldToScreenPoint (transform.position);
+            mouse_pos.x = mouse_pos.x - object_pos.x;
+            mouse_pos.y = mouse_pos.y - object_pos.y;
+            
+            //rigidbody2D.transform.rotation = transform.rotation;
+            rigidbody2D.AddForce (new Vector2 (mouse_pos.x * 500f,
+                                               mouse_pos.y * 500f));
+        }
     }
 
     // Update is called once per frame
