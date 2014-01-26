252a849f0400b3f320989930bcec06728e8ab0fe
BobSmun <somemadeupname@gmail.com>
2014-01-26 11:48:02 +0800

    Improve slash input detection

--- Assets/Scripts/UnitChaser.cs
+++ Assets/Scripts/UnitChaser.cs
@@ -3,76 +3,87 @@ using System.Collections;
 
 public class UnitChaser : Unit
 {
-		public GameObject gameController;
-		protected float _stunTime = 3.0f;
-		private bool prevStrike;
-		private bool isNotFucked = true;
+    public GameObject gameController;
+    protected float _stunTime = 3.0f;
+    private int Strike = 0;
+    private bool isNotFucked = true;
 
-		// Use this for initialization
-		public override void Start ()
-		{
-				gameController = GameObject.Find ("GameController");
-				base.Start ();
-		}
+    // Use this for initialization
+    public override void Start()
+    {
+        gameController = GameObject.Find("GameController");
+        base.Start();
+    }
 
-		[RPC] 
-		void DisableEnemy ()
-		{
-				isNotFucked = false;
-		}
+    [RPC] 
+    void DisableEnemy()
+    {
+        isNotFucked = false;
+    }
 	
-		[RPC]
-		void EnableEnemy ()
-		{
-				isNotFucked = true;
-		}
+    [RPC]
+    void EnableEnemy()
+    {
+        isNotFucked = true;
+    }
 
-		void OnGUI ()
-		{
-				//move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-				//GUI.TextArea(new Rect(100,100,100,100), string.Format("Move X: {0}\n Move Y: {1}", move.x, move.y));
-		}
+    void OnGUI()
+    {
+        //move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        //GUI.TextArea(new Rect(100,100,100,100), string.Format("Move X: {0}\n Move Y: {1}", move.x, move.y));
+    }
     
-		[RPC]
-		void SetStrike (int strike)
-		{
-				if (anim) {
-						anim.SetBool ("Strike", strike != 0);
-				}
-		}
+    [RPC]
+    void SetStrike(int strike)
+    {
+        if(anim)
+        {
+            anim.SetBool("Strike", strike != 0);
+        }
+    }
 
-		private void UpdateStunnedStuff ()
-		{
-				_stunTime -= Time.deltaTime;
+    private void UpdateStunnedStuff()
+    {
+        _stunTime -= Time.deltaTime;
 		
-				if (_stunTime < 0.0f) {
-						isNotFucked = true;
-						_stunTime = 5.0f;
-				}
-		}
-		// Update is called once per frame
-		public override void Update ()
-		{
-				if (GetComponent<NetworkView> ().isMine &&
-						gameController.GetComponent<GameController> ().InputEnabled && isNotFucked) {
-						//Movement shiat
-						move = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
-						move.Normalize ();
+        if(_stunTime < 0.0f)
+        {
+            isNotFucked = true;
+            _stunTime = 5.0f;
+        }
+    }
+    // Update is called once per frame
+    public override void Update()
+    {
+        if(GetComponent<NetworkView>().isMine &&
+            gameController.GetComponent<GameController>().InputEnabled && isNotFucked)
+        {
+            //Movement shiat
+            move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            move.Normalize();
 
-						bool strike = Input.GetMouseButtonDown (0);
+            if(Strike != 0)
+            {
+                --Strike;
+            }
+            else
+            {
+                Strike = Input.GetMouseButtonDown(0) ? 6 : 0;
+            }
 
-						if (anim && (prevStrike != strike)) {
-								//Swing Weapon Here
-								anim.SetBool ("Strike", strike);
-								networkView.RPC ("SetStrike", RPCMode.AllBuffered, strike ? 2 : 0);
-								prevStrike = strike;
-						}
+            if(anim && Strike >= 0)
+            {
+                //Swing Weapon Here
+                anim.SetBool("Strike", Strike != 0);
+                networkView.RPC("SetStrike", RPCMode.AllBuffered, Strike);
+            }
 
-						base.Update ();
-				}
+            base.Update();
+        }
 
-				if (!isNotFucked) {
-						UpdateStunnedStuff ();
-				}
-		}
+        if(!isNotFucked)
+        {
+            UpdateStunnedStuff();
+        }
+    }
 }
